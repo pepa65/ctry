6f85282e47ae708e5a83f42d81325c19072ba79d
gitlab.com/pepa65 <pepa65@passchier.net>
2026-08-26 15:55:12 +0700

    Migrate to clap 4

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,92 @@
-mod retry;
-
 use std::time::Duration;
 
-#[macro_use]
-extern crate clap;
-use clap::{App, AppSettings, Arg};
+use clap::{Arg, Command};
 
+mod retry;
 use retry::retry;
 
 fn main() {
-	let matches = App::new("ctry")
-		.version(crate_version!())
-		.about(crate_description!())
+	let matches = Command::new("ctry")
+		.version(env!("CARGO_PKG_VERSION"))
+		.about(env!("CARGO_PKG_DESCRIPTION"))
+		.help_template(
+			"{name} {version} - {about}\n\
+			{usage-heading} {usage}\n\
+			{all-args}{after-help}")
+		.before_help(format!("ctry {} - Retry CLI commands", env!("CARGO_PKG_VERSION")))
 		.arg(
-			Arg::with_name("max")
-				.short("m")
+			Arg::new("max")
+				.short('m')
 				.long("max")
 				.value_name("MAX_RETRIES")
 				.help("Maximum retries, use 0 for unlimited retries")
-				.takes_value(true)
-				.default_value("5"),
+				.num_args(1)
+				.default_value("5")
 		)
 		.arg(
-			Arg::with_name("interval")
-				.short("i")
+			Arg::new("interval")
+				.short('i')
 				.long("interval")
 				.value_name("INTERVAL")
 				.help("Interval in seconds between the retries")
-				.takes_value(true)
-				.default_value("1"),
+				.num_args(1)
+				.default_value("1")
 		)
 		.arg(
-			Arg::with_name("exit_code")
-				.short("e")
+			Arg::new("exit_code")
+				.short('e')
 				.long("exitcode")
 				.value_name("EXIT_CODE")
 				.help("On which exit code retries will stop")
-				.takes_value(true)
-				.default_value("0"),
+				.num_args(1)
+				.default_value("0")
 		)
-		.arg(Arg::with_name("quiet").short("q").long("quiet").help("Suppress output of the command"))
-		.setting(AppSettings::TrailingVarArg)
-		.arg(Arg::with_name("command").value_name("COMMAND").help("Command to run").required(true).multiple(true).last(true))
+		.arg(
+			Arg::new("quiet")
+				.short('q')
+				.long("quiet")
+				.help("Suppress output of the command")
+				.action(clap::ArgAction::SetTrue)
+		)
+		.trailing_var_arg(true)
+		.arg(
+			Arg::new("command")
+				.value_name("COMMAND")
+				.help("Command to run")
+				.required(true)
+				.num_args(1..)
+		)
 		.get_matches();
-	let max_retries: u32 = match matches.value_of("max").unwrap_or_default().parse() {
-		Ok(u) => u,
-		Err(_) => panic!("The given MAX option must be an Integer"),
+
+	let max_retries: u32 = matches
+		.get_one::<String>("max")
+		.unwrap()
+		.parse()
+		.expect("The given MAX option must be an Integer");
+	let interval = Duration::from_secs(
+		matches
+			.get_one::<String>("interval")
+			.unwrap()
+			.parse()
+			.expect("The given INTERVAL option must be an Integer"),
+	);
+	let exitcode = matches
+		.get_one::<String>("exit_code")
+		.unwrap()
+		.parse()
+		.expect("The given exit code option must be an Integer");
+	let quiet = matches.get_flag("quiet");
+	let cmd: Vec<&str> = matches
+		.get_many::<String>("command")
+		.unwrap()
+		.map(String::as_str)
+		.collect();
+	let config = retry::RetryConfig {
+		max: max_retries,
+		interval,
+		expected_exitcode: exitcode,
+		quiet,
+		cmd,
 	};
-	let interval = Duration::from_secs(match matches.value_of("interval").unwrap_or_default().parse() {
-		Ok(u) => u,
-		Err(_) => panic!("The given INTERVAL option must be an Integer"),
-	});
-	let exitcode = match matches.value_of("exit_code").unwrap_or_default().parse() {
-		Ok(c) => c,
-		Err(_) => panic!("The given exit code option must be an Integer"),
-	};
-	let quiet = matches.is_present("quiet");
-	let cmd: Vec<&str> = matches.values_of("command").unwrap().collect();
-	let config = retry::RetryConfig { max: max_retries, interval, expected_exitcode: exitcode, quiet, cmd };
 	retry(config);
 }
